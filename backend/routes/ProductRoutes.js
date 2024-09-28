43451d995c87678c9926fe9a0db5f4978780bfa2
Mariam0Gafar <mariam.gaafar02@gmail.com>
2024-09-29 02:05:52 +0300

    changed product routes

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {getProducts, createProduct, searchProductsByName} = require ('../controllers/ProductController');
 
-router.get ('/', getProducts);
-router.post ('/create', createProduct);
-router.get ('/search', searchProductsByName);
+router.get ('/products', getProducts);
+router.post ('/products', createProduct);
+router.get ('/products', searchProductsByName);
 
 module.exports = router;
